fcee401e3782b88f78011292a3558b29181ad705
Francisco Javier <franciscoinzunza2006@gmail.com>
2026-07-19 17:35:50 -0700

    Arrow movements

--- src/main.c
+++ src/main.c
@@ -1,18 +1,8 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
 
 #include "raylib.h"
-
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
-int main ()
-{
+int main () {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
@@ -21,15 +11,32 @@ int main ()
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
-
 	// Load a texture from the resources directory
+	Texture the_batter = LoadTexture("spr_batter_down.png");
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
+
+	Vector2 position = {0};
+	float speed = 1.0f;
+
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
+		const bool up = IsKeyDown(KEY_UP);
+		const bool down = IsKeyDown(KEY_DOWN);
+		const bool left = IsKeyDown(KEY_LEFT);
+		const bool right = IsKeyDown(KEY_RIGHT);
+
+		const int horizontal_direction = right - left;
+		const int vertical_direction = down - up;
+
+		position.x += horizontal_direction * speed;
+		position.y += vertical_direction * speed;
+
 		// drawing
 		BeginDrawing();
+
+		DrawRectangle((int)position.x, (int)position.y, 32, 32, RED);
+
+		//DrawTexture(the_batter, position, 100, WHITE);
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
@@ -46,6 +53,7 @@ int main ()
 
 	// cleanup
 	// unload our texture so it can be cleaned up
+	UnloadTexture(the_batter);
 	UnloadTexture(wabbit);
 
 	// destroy the window and cleanup the OpenGL context
